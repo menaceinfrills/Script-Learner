23f85b2b982a6dd6030341050b0784fcd935afd1
N <menaceinfrills@tutanota.com>
2024-08-06 17:24:38 +0200

    ex 3

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,55 @@
 module Main exposing (..)
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
-
-
 -- MAIN
-
-
 main = Browser.sandbox { init = init, update = update, view = view }
 
 -- MODEL
 type alias Model =
-  { content : String
+  { name : String
+  , password : String
+  , passwordAgain : String
   }
-
 
 init : Model
-init =
-  { content = ""
-  }
+init = Model "" "" ""
 
 -- UPDATE
-type Msg = Change String
+type Msg = Name String
+         | Password String
+         | PasswordAgain String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Change newContent -> { model | content = newContent }
+    Name name              -> { model | name = name }
+    Password password      -> { model | password = password }
+    PasswordAgain password -> { model | passwordAgain = password }
 
 -- VIEW
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
-    , div [] [ text (String.reverse model.content), text (", length: " ++ String.fromInt (String.length model.content)) ]
+    [ viewInput "text" "Name" model.name Name
+    , viewInput "password" "Password" model.password Password
+    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+    , viewValidation model
     ]
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+viewValidation : Model -> Html msg
+viewValidation model =
+
+
+  if      not (String.any Char.isUpper model.password) then div [ style "color" "red" ] [ text "Password has no uppercase letters!" ]
+  else if not (String.any Char.isLower model.password) then div [ style "color" "red" ] [ text "Password has no lowercase letters!" ]
+  else if not (String.any Char.isDigit model.password) then div [ style "color" "red" ] [ text "Password has no numeric characters!" ]
+  else if (String.length model.password) < 8           then div [ style "color" "red" ] [ text "Password too short!" ]
+  else if model.password /= model.passwordAgain        then div [ style "color" "red" ] [ text "Passwords do not match!" ]
+  else                                                      div [ style "color" "green" ] [ text "OK" ]
