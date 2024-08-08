882e819e6c02af45681adeddbe2e89432c96cc78
N <menaceinfrills@tutanota.com>
2024-08-08 19:37:06 +0200

    refactoring

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,10 @@
 module Main exposing (main)
 
 import Browser
-import Browser.Dom
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Maybe
-import Task
-
 
 main : Program () Model Msg
 main = Browser.sandbox { view          = view
@@ -15,39 +12,32 @@ main = Browser.sandbox { view          = view
                        , update        = update
                        }
 
---Datatype
-type alias Model = { selected : Maybe String
-                   , ids      : List String
+--Datatypes
+type alias Model = { selected : Maybe Int
+                   , ids      : List Int
                    }
 
-type Msg = NoOp
-         | Focus (Maybe String)
+type Msg = Focus Int
 
 --Init
 init : Model
-init =  initialModel
-
-initialModel : Model
-initialModel = { selected = Nothing
-               , ids      = List.map String.fromInt (List.range 1 10) }
+init = { selected = Nothing
+       , ids      = (List.range 1 10) }
 
 -- Update
 update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        NoOp                   -> model
-        Focus (Just selected)  -> { model | selected = Just selected }
-        Focus Nothing          -> { model | selected = Nothing }
+update msg model = case msg of
+                   Focus id  -> { model | selected = Just id }
 
 -- View
 view : Model -> Html Msg
-view model = div [] [ div [] [ text ("Currently selected: " ++ (Maybe.withDefault "" model.selected)) ]
-                    , div [] (List.map viewButton model.ids)
-                    , div [] (List.map viewInput model.ids)
+view model = div [] [ div [] (List.map viewInput model.ids)
+                    , div [] [ text ("Currently selected: " ++ (showID model.selected)) ]
                     ]
 
-viewButton : String -> Html Msg
-viewButton id = button [ onClick (Focus (Just id)) ] [ text id ]
+showID : Maybe Int -> String
+showID arg = Maybe.withDefault ""<| Maybe.map String.fromInt <| arg
 
-viewInput : String -> Html Msg
-viewInput idstr = div [] [ input [ id idstr, placeholder idstr, onFocus (Focus (Just idstr)) ] [] ]
+viewInput : Int -> Html Msg
+viewInput idn = let idstr = String.fromInt idn
+                in div [] [ input [ id idstr, placeholder idstr, onFocus (Focus idn) ] [] ]
